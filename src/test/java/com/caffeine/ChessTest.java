ef89a585bf8c496e7927ad89d5306cc052a08f4b
Jeff Warner <jeff@develops.software>
2016-09-24 17:38:12 -0400

    Walking Skeleton ready

---
+++ src/test/java/com/caffeine/ChessTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.caffeine.Chess;
+
+public class ChessTest {
+
+    @Test
+    public void testExitWithoutError(){
+        String phrase = "Our Chess is The One True Chess.";
+        assertEquals(Chess.echo(phrase), phrase);
+    }
+}
