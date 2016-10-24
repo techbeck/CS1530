e0202a4276b0e4ae1dc008c075c05ac933b4c0c1
Jeff Warner <jeff@develops.software>
2016-10-24 17:00:18 -0400

    Integrated JockfishEngine from jwarner112

--- src/test/java/com/caffeine/ChessTest.java
+++ src/test/java/com/caffeine/ChessTest.java
@@ -1,6 +1,11 @@
+// First-Party Imports
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+// Third-Party Imports
+import com.jwarner.jockfish.JockfishEngine;
+
+// Local Imports
 import com.caffeine.Chess;
 
 public class ChessTest {
@@ -9,5 +14,13 @@ public class ChessTest {
     public void testExitWithoutError(){
         String phrase = "Our Chess is The One True Chess.";
         assertEquals(Chess.echo(phrase), phrase);
+    }
+
+    @Test
+    public void testJockfishEngineIntegrated(){
+        String expected = "Stockfish 7 64 by T. Romstad, M. Costalba, J. Kiiski, G. Linscott\n";
+        JockfishEngine jockfish = new JockfishEngine();
+        String observed = jockfish.read();
+        assertTrue(expected.equals(observed));
     }
 }
