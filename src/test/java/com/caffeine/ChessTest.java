6f7c3d1b5facb7efa57903508dc88924504a73cb
Daniel Brusilovsky <dpb32@pitt.edu>
2016-10-26 13:07:21 -0400

    Working on GUI Tests

--- src/test/java/com/caffeine/ChessTest.java
+++ src/test/java/com/caffeine/ChessTest.java
@@ -1,15 +1,40 @@
 // First-Party Imports
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import javax.swing.*;
+import java.awt.Frame;
+
 
 // Third-Party Imports
 import com.jwarner.jockfish.JockfishEngine;
+import org.assertj.swing.core.*;
+import org.assertj.swing.fixture.*;
+import org.assertj.swing.testing.AssertJSwingTestCaseTemplate;
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.swing.launcher.ApplicationLauncher.application;
+import static org.assertj.swing.finder.WindowFinder.findFrame;
 
 // Local Imports
 import com.caffeine.Chess;
 
 public class ChessTest {
+    public FrameFixture frame;
+    @BeforeClass
+    public static void setUp() {
+        application(Chess.class).start();
+    }
+
+    @Test
+    public void testMyFaithInGod(){
+        Robot robot = BasicRobot.robotWithNewAwtHierarchy();
+        frame = findFrame()
+        frame = findFrame(new GenericTypeMatcher<Frame>(Frame.class) {
+            protected boolean isMatching(Frame frame) {
+                return "Laboon Chess".equals(frame.getTitle()) && frame.isShowing();
+            }
+        }).using(robot);
+    }
 
     @Test
     public void testJockfishEngineIntegrated(){
