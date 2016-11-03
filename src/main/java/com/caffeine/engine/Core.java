78d8f8cfb0e90982f24d921c22c75cd83f50cbdd
Jeff Warner <jeff@develops.software>
2016-11-02 23:48:50 -0400

    Saving progress

--- src/main/java/com/caffeine/engine/Core.java
+++ src/main/java/com/caffeine/engine/Core.java
@@ -3,6 +3,10 @@ package com.caffeine.engine;
 // First-Party Libs
 import java.util.*;
 
+// Third-Party Libs
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+
 // Local Libs
 import com.jwarner.jockfish.JockfishEngine;
 
@@ -28,9 +32,7 @@ public class Core{
 
     public void go(String type, String[] parameters){
         String cmd = String.format("go %s ", type);
-        for (int i = 0; i < parameters.length; i++){
-            cmd.concat(parameters[i] + " ");
-        }
+        for (int i = 0; i < parameters.length; i++){ cmd.concat(parameters[i] + " "); }
         jockfish.write(cmd);
     }
 
@@ -38,58 +40,113 @@ public class Core{
         jockfish.write("stop");
     }
 
-    // Write a UCI method that returns a Dict of values representing:
-    //
-    // ID:
-    // AUTHOR:
-    // OPTIONS:
-    //     WRITE:
-    //     CONTEMPT:
-    //     THREADS:
-    //     HASH:
-    //     CLEAR:
-    //     PONDER:
-    //     MULTIPV:
-    //     SKILL-LEVEL:
-    //     MOVE-OVERHEAD:
-    //     MINIMUM-THINKING-TIME:
-    //     SLOW-MOVER:
-    //     NODES-TIME:
-    //     UCI-CHESS960:
-    //     SYZYGY-PATH:
-    //     SYZYGY-PROBE-DEPTH:
-    //     SYZYGY-50-MOVE-RULE:
-    //     SYZYGY-PROBE-LIMIT:
-
-// TODO: WRITE JOCKFISH UNIT TESTS
-// TODO: UPGRADE JOCKFISH ENGINE LOCALLY
-
-    public HashMap<String, Object> uci(){
-        String res;
-        String[] resArr;
-        HashMap<String, Object> result = new HashMap<String, Object>();
-        HashMap<String, String> options = new HashMap<String, String>();
-
+    public HashMap<String, String> getConfig(){
+        HashMap<String, String> result = new HashMap<String, String>();
         jockfish.write("uci");
-
-        // Turn string responses into an accessible dictionary-like mapping.
-        loopReadLines: while(true){
-            res = jockfish.readLine().trim();
-            resArr = res.split(" ");
-            switch (resArr[0]){
-                case "id":
-                    result.put(resArr[1], Arrays.copyOfRange(resArr, 2, resArr.length).toString());
-                    break;
-                case "options":
-                    options.put(resArr[2], Arrays.copyOfRange(resArr, 3, resArr.length).toString());
-                    break;
-                case "uciok":
-                    break loopReadLines;
-                default:
-                    break;
+        String responseLine;
+        while (true) {
+            responseLine = jockfish.readLine().trim();
+            if (responseLine.equals("uciok")) { break; }
+            if (responseLine.contains("option name")){
+                String[] items = responseLine.split(" ", 4);
+                result.put(items[2], items[3]);
             }
         }
-        result.put("options", options);
         return result;
+    }
+
+    public void setFEN(String fen){
+        jockfish.write(String.format("position", fen));
+    }
+
+    private boolean isValid(String fen){
+        /*  Assert that properties of the String are true, else return false.
+            Properties can be found at:
+              https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
+        */
+        String validOptions;
+
+        //  FEN String cannot be blank.
+        if (StringUtils.isBlank(fen)){ return false; }
+
+        //  FEN String has exactly 6 fields separated by spaces.
+        String[] fenFields = fen.trim().split(" ");
+        if (fenFields.length != 6){ return false; }
+
+        //  FEN Field 1 is Piece Placement from Rows 8 to 1 delimited by '/'.
+        //    Player 1 (White)'s Pieces are within "PNBRQK",
+        //    Player 2 (Black)'s Pieces are within "pnbrqk",
+        //    Empty squares grouped as digits within [1,8] (incl.)
+        validOptions = "pnbrqk";
+        String[] boardState = fenFields[0].split("/");
+        if (boardState.length != 8){ return false; }
+        for (String str : boardState){
+            int rowValue = 0; // Digits are their value. Letters are a 1.
+            for (char ch : str.toLowerCase().toCharArray()){
+                if (validOptions.contains(ch)){
+                    rowValue += CharUtils.toIntValue(ch, 1);
+                } else { return false; }
+            }
+            if (rowValue != 8){ return false; }
+        }
+
+        //  FEN Field 2: Active Color. Can only be "w" or "b".
+        validOptions = "wb";
+        String activePlayer = fenFields[1];
+        if (activePlayer.length != 1){ return false; }
+        if (!validOptions.contains(activePlayer)){ return false; }
+
+        //  FEN Field 3: Castling Availability of Kings and Queens; "KQkq".
+        validOptions = "KQkq";
+        String castlingAbility = fenFields[2];
+        // No more than 4 chars, e.g. "KQkq-" is invalid.
+        if (castlingAbility.length > 4){ return false; }
+        // If '-', nobody can castle and we can skip all this.
+        if (!castlingAbility.equals("-")){
+            int uniqueCount = 0;
+            String uniqueChars;
+            for (char ch : castlingAbility.toCharArray()){
+                // Check all chars are valid.
+                if (!validOptions.contains(ch)){ return false; }
+                // Check all chars are unique. "KKkq" is invalid.
+                if (!uniqueChars.contains(ch)){ uniqueChars = ch + uniqueChars; }
+            }
+            if (uniqueChars.length != castlingAbility.length){ return false; }
+        }
+
+        // FEN Field 4: En Passant, can be either '-' or a board location.
+        String enPassantStatus = fenFields[3].toLowerCase();
+        // If '-', no en-passant and we can skip all this.
+        if (!enPassantStatus.equals("-")){ return false; }
+        if (enPassantStatus.length() != 2){ return false; }
+        char[] enPassantArray = enPassantStatus.toCharArray();
+        // If not '-' it has to be a char in [a-h] followed by a digit in [1-8]
+        validOptions = "abcdefgh";
+        if (!validOptions.contains(enPassantArray[0])){ return false; }
+        validOptions = "12345678";
+        if (!validOptions.contains(enPassantArray[1])){ return false; }
+
+        // FEN Field 5: Halfmove Clock (Ply Count since last pawn move or capture).
+        String halfmoveClock = fenFields[4];
+        // Must be non-negative integer.
+        try {
+            int halfmoveVal = Integer.parseInt(halfmoveClock);
+            if (halfmoveVal < 0){ return false; }
+        } catch (Exception e){ return false; } // If string isn't a number
+
+        // FEN Field 6: Fullmove Clock (Full turns in the game).
+        String fullmoveClock = fenFields[5];
+        // Must be non-negative.
+        try {
+            int fullmoveVal = Integer.parseInt(fullmoveClock);
+            if (fullmoveVal < 0){ return false; }
+        } catch (Exception e){ return false; } // If string isn't a number
+
+        // NOTE: Can't easily validate halfmoveVal and fullmoveVal in
+        //       relation to one another because of the start/endgame
+        //       edge cases.
+
+        // If we're still here, we've most likely got a valid FEN String!
+        return true;
     }
 }
--- src/test/java/com/caffeine/engine/CoreTest.java
+++ src/test/java/com/caffeine/engine/CoreTest.java
@@ -4,6 +4,7 @@ import java.util.*;
 // Third-Party Libs
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.apache.commons.lang3.StringUtils;
 
 // Local Libs
 import com.caffeine.engine.Core;
@@ -19,17 +20,45 @@ public class CoreTest {
     }
 
     @Test
-    public void testCoreUCI(){
+    public void testCoreGetConfig(){
         Core engine = new Core();
-        String trash = engine.out(); // Eat greeting string.
-        HashMap<String, Object> engineConfig = engine.uci();
-        for (Map.Entry<String, Object> entry : engineConfig.entrySet()){
-            if (entry.getKey().equals("options")){
-                for (Map.Entry<String, String> fntry : entry.getValue().entrySet()){
-                    System.out.println("\t"+fntry.getKey()+" "+fntry.getValue());
-                }
-            } else {
-                System.out.print(String.format("%s: %s\n", entry.getKey(), entry.getValue()));
+
+        HashMap<String, String> response = engine.getConfig();
+
+        // Define Options as initialized in a new Stockfish instance
+        List<List<String>> options = new List<List<String>>();
+        options.add(["Write", "Debug Log type check default false"]);
+        options.add(["Contempt", "type spin default 0 min -100 max 100"]);
+        options.add(["Threads", "type spin default 1 min 1 max 128"]);
+        options.add(["Hash", "type spin default 16 min 1 max 1048576"]);
+        options.add(["Clear", "Hash type button"]);
+        options.add(["Ponder", "type check default false"]);
+        options.add(["MultiPV", "type spin default 1 min 1 max 500"]);
+        options.add(["Skill", "Level type spin default 20 min 0 max 20"]);
+        options.add(["Move", "Overhead type spin default 30 min 0 max 5000"]);
+        options.add(["Minimum", "Thinking Time type spin default 20 min 0 max 5000"]);
+        options.add(["Slow", "Mover type spin default 84 min 10 max 1000"]);
+        options.add(["nodestime", "type spin default 0 min 0 max 10000"]);
+        options.add(["UCI_Chess960", "type check default false"]);
+        options.add(["SyzygyPath", "type string default <empty>"]);
+        options.add(["SyzygyProbeDepth", "type spin default 1 min 1 max 100"]);
+        options.add(["Syzygy50MoveRule", "type check default true"]);
+        options.add(["SyzygyProbeLimit", "type spin default 6 min 0 max 6"]);
+
+        for (String[] option : options){
+            String optString = response.get(option[0]);
+            if (StringUtils.isBlank(optString)){
+                fail("Did not find option '"+optString+"'.");
+            }
+            if (!optString.equals(option[1])){
+                String failString = String.format(
+                    "Option did not match default.\n" +
+                    "  DEFAULT: '%s'\n" +
+                    "  FOUND:   '%s'\n",
+                    option[1],
+                    optString
+                );
+                fail(failString);
             }
         }
     }
