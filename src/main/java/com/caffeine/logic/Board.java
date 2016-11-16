f51859f279f112a1c9d31deabfb3ba1bac1d632f
Jeff Warner <jeff@develops.software>
2016-11-16 13:46:44 -0500

    Fixed initializers. Still needs unit tests.

--- src/main/java/com/caffeine/logic/Board.java
+++ src/main/java/com/caffeine/logic/Board.java
@@ -19,9 +19,9 @@ public class Board {
     // Constructors
     public Board(Piece[][] pieceArray){ pieces = pieceArray; }
 
-    public Board(){ this(Board.getEmptyBoard()); }
+    public Board(String fen){ this(Board.pieceArrayFromFEN(fen)); }
 
-    public Board(String fenString){ this(pieceArrayFromFEN(fenString)); }
+    public Board(){ this(Board.getEmptyBoard()); }
 
 
 
@@ -52,16 +52,76 @@ public class Board {
         }
     }
 
-    public Piece[][] pieceArrayFromFEN(String fen) throws IllegalArgumentException {
-        if (!Utils.isValidFEN(fen)){
 
-            String exMsg = "Invalid FEN was encountered.";
-            throw new IllegalArgumentException(exMsg);
+    public String toFENPartial(){
+        // Creates a String of the format 8/7/6/5/4/3/2/1 where:
+        //     - Each '/'' separates a 'row' String meeting the criteria below.
+        //     - The digit in the above corresponds to that row on a ches board.
+        //     - Each row is composed only of the groups [PRNBQKprnbqk], and [1-8].
+        //     - The length of a row cannot exceed 8 chars.
+        //     - For each row,
+        //          - Assign pt value '1' to character matching in [PRNBQKprnbqk].
+        //          - Assign pt value equal to any digit in [1-8].
+        //          - The sum of all pt values cannot exceed 8.
+        //          - e.g. "2QP3r" = 2 + 1 + 1 + 3 + 1 = 8.
+        String result;
+        String sep = "/";
+        Piece currentPiece;
+        int lineCursor;
 
-        } else {
+        result = "";
+        for (int i = 0; i < 8; i++){ // Iterate over board's rows
+            lineCursor = 0;
+            for (int j = 0; j < 8; ){ // Iterate over board's columns
+                do {
+                    currentPiece = getPiece(Utils.translateCoordinate(new Integer[]{i, j}));
+                    lineCursor++;
+                } while(currentPiece == null);
+                if (lineCursor > 1){ result += Integer.toString(lineCursor); }
+                result += currentPiece.getClass().toString();
+                j += lineCursor;
+            }
+            result += sep;
+        }
+        result = StringUtils.chop(result); // Remove trailing '/'
 
-            // Default: Board squares w/ no piece are null
-            Piece[][] pieces = getEmptyBoard();
+        return result;
+    }
+
+    public String toString(){
+
+        // Creates a multi-line string depicting the board in ASCII.
+        // Same as how Stockfish generates it.
+        String result;
+        String sep = " +---+---+---+---+---+---+---+---+";
+        String pieceTemplate = " %c |";
+        String currentRowString;
+
+        result = "";
+        for (int i = 0; i < 8; i++){ // Iterate over rows
+            // Build the row's string, append it to the board.
+            currentRowString = "";
+            for (int j = 0; j < 8; j++){ // Iterate over columns
+                Piece pc = getPiece(Utils.translateCoordinate(new Integer[]{i, j}));
+                currentRowString += String.format(" %c |", pc.getType());
+            }
+            result += String.format("%s\n | %s\n", currentRowString);
+        }
+        result += sep;
+
+        return result;
+
+    }
+
+
+
+    // Private Methods
+    private static Piece[][] pieceArrayFromFEN(String fen) {
+
+        // Default: Board squares w/ no piece are null
+        Piece[][] pieces = Board.getEmptyBoard();
+
+        if (Utils.isValidFEN(fen)){
 
             // Get board as String[8] where [0] is row 8 and [7] is row 1.
             String[] board = fen.split(" ", 2)[0].split("/");
@@ -93,68 +153,15 @@ public class Board {
         }
 
         return pieces;
+
     }
 
-    public String toFENPartial(){
-        // Creates a String of the format 8/7/6/5/4/3/2/1 where:
-        //     - Each '/'' separates a 'row' String meeting the criteria below.
-        //     - The digit in the above corresponds to that row on a ches board.
-        //     - Each row is composed only of the groups [PRNBQKprnbqk], and [1-8].
-        //     - The length of a row cannot exceed 8 chars.
-        //     - For each row,
-        //          - Assign pt value '1' to character matching in [PRNBQKprnbqk].
-        //          - Assign pt value equal to any digit in [1-8].
-        //          - The sum of all pt values cannot exceed 8.
-        //          - e.g. "2QP3r" = 2 + 1 + 1 + 3 + 1 = 8.
-        String result;
-        String sep = "/";
-        Piece currentPiece;
-        int lineCursor;
-
-        result = "";
-        for (int i = 0; i < 8; i++){ // Iterate over board's rows
-            for (int j = 0; j < 8; ){ // Iterate over board's columns
-                do {
-                    currentPiece = getPiece(Utils.translateCoordinate(new Integer[]{i, j}));
-                    lineCursor++;
-                } while(currentPiece == null);
-                if (lineCursor > 1){ result += Integer.toString(lineCursor); }
-                result += currentPiece.getClass().toString();
-                j += lineCursor;
-            }
-            result += sep;
-        }
-        result = StringUtils.chop(result); // Remove trailing '/'
-    }
-
-    public String toString(){
-        // Creates a multi-line string depicting the board in ASCII.
-        // Same as how Stockfish generates it.
-        String result;
-        String sep = " +---+---+---+---+---+---+---+---+";
-        String pieceTemplate = " %c |";
-        String currentRowString;
-
-        result = "";
-        for (int i = 0; i < 8; i++){ // Iterate over rows
-            // Build the row's string, append it to the board.
-            currentRowString = "";
-            for (int j = 0; j < 8; j++){ // Iterate over columns
-                Piece pc = getPiece(Utils.translateCoordinate(new Integer[]{i, j}));
-                currentRowString += String.format(" %c |", pc.getType());
-            }
-            result += String.format("%s\n | %s\n", currentRowString);
-        }
-        result += sep;
-        return result;
-    }
-
-
-
-    // Private Methods
     private static Piece[][] getEmptyBoard(){
+
         Piece[][] emptyBoard = new Piece[8][8];
         for (int i = 0; i < 8; i++){ Arrays.fill(emptyBoard[i], null); }
+
         return emptyBoard;
+
     }
 }
